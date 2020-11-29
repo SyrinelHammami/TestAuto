e469bb6c7d14c670bf681c7c1567df7cd76ce3ba
cyrina-github <hammamicyrine74@gmail.com>
2020-11-29 12:03:13 +0100

    this is cyrine first commit

---
+++ DemoTestAuto/src/test/java/DemoTest.java
@@ -0,0 +1,17 @@
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELLO WORLD");
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+	}
